4b771dc6717477afb8a421cb945b3a1eb6a196e9
Pradhumna Pancholi <pnpancholi6629.pnp@gmail.com>
2019-06-11 23:55:23 -0400

    added CircleImgView

---
+++ Navigator/CircleImgView.swift
@@ -0,0 +1,28 @@
+//
+//  CircleImgView.swift
+//  Navigator
+//
+//  Created by Pradhumna Pancholi on 2019-06-11.
+//  Copyright © 2019 Pradhumna Pancholi. All rights reserved.
+//
+
+import SwiftUI
+
+struct CircleImgView : View {
+    var body: some View {
+        Image("niagaraFalls")
+        .clipShape(Circle())
+        .overlay(
+            Circle().stroke(Color.white)
+        )
+        .shadow(radius: 10)
+    }
+}
+
+#if DEBUG
+struct CircleImgView_Previews : PreviewProvider {
+    static var previews: some View {
+        CircleImgView()
+    }
+}
+#endif
